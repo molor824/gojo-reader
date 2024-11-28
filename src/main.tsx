92b7fdf1fcc8bdf058d8406ed561bc88986545a3
Molor Margad-Erdene <molormargaderdene@gmail.com>
2024-11-28 09:34:54 +0800

    feat: add error page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import { Home } from "./pages/home/Home";
 import "./index.css";
+import { Error } from "./pages/error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
     children: [{ path: "", element: <Home /> }],
   },
 ]);
