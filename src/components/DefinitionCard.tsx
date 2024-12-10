4432690a9684780851d8334455d651eb2d4a18b3
molor824 <molormargaderdene@gmail.com>
2024-12-10 19:06:56 +0800

    fix: complete word definition card

--- src/components/DefinitionCard.tsx
+++ src/components/DefinitionCard.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 type Meaning = {
-  tag?: string;
+  tag: string;
   meaning: string;
 };
 type Definition = {
@@ -15,8 +15,8 @@ type Definition = {
 
 const PARSER = new DOMParser();
 
-function fetchDefinition(word: string) {
-  return fetch(`https://jisho.org/api/v1/search/words?keyword=${word}`)
+const fetchDefinition = (word: string) =>
+  fetch(`https://jisho.org/word/${word}`)
     .then((res) =>
       res.status === 200
         ? res.text()
@@ -42,12 +42,12 @@ function fetchDefinition(word: string) {
         let meaning = meaningElement.textContent;
         let tag = meaningTagsElements[index].textContent;
         if (!meaning) throw new Error("No meaning found");
+        if (!tag) throw new Error("No tag found");
         return { tag, meaning };
       });
 
-      return Promise.resolve({});
+      return Promise.resolve<Definition>({ furigana, meanings });
     });
-}
 
 // Cache used for storing the word definition data, that way you don't have to fetch the same data multiple times
 const cache: Record<string, Definition> = {};
@@ -57,7 +57,42 @@ export const DefinitionCard = ({ word }: Props) => {
 
   useEffect(() => {
     if (cache[word]) setDefinition(cache[word]);
+    else {
+      setDefinition(undefined);
+      fetchDefinition(word)
+        .then((def) => {
+          cache[word] = def;
+          setDefinition(def);
+        })
+        .catch(console.error);
+    }
   }, [word]);
 
-  return <div></div>;
+  return (
+    <div className="w-full rounded-lg bg-gray-800 text-white p-4">
+      {definition ? (
+        <div className="flex flex-col gap-4">
+          <h1 className="flex">
+            {definition.furigana.map((furigana, index) => (
+              <div className="flex flex-col items-center" key={index}>
+                <span className="text-[10px]">{furigana}</span>
+                <span className="text-xl">{word.charAt(index)}</span>
+              </div>
+            ))}
+          </h1>
+          {definition.meanings.map(({ meaning, tag }, index) => (
+            <div key={index} className="flex gap-2">
+              <span>{index + 1}. </span>
+              <div>
+                <span className="text-gray-400 text-sm">{tag}</span>
+                <p>{meaning}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-2xl text-gray-400">Loading...</p>
+      )}
+    </div>
+  );
 };
