e95097d9f4a270cbf0ed0faa34b2b93d7f0fcc7e
molor824 <molormargaderdene@gmail.com>
2024-12-12 15:24:07 +0800

    fix: use vercel serverless function

--- api/jisho.js
+++ api/jisho.js
@@ -1,18 +1,6 @@
-import express from "express";
-import ViteExpress from "vite-express";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const distPath = path.join(__dirname, "../dist");
-
-const PORT = 3000;
-
-const app = express();
 const cache = {};
 
-app.get("/api/jisho", (req, res) => {
+export default function handler(req, res) {
   const word = req.query.word;
 
   console.log(word);
@@ -30,8 +18,4 @@ app.get("/api/jisho", (req, res) => {
         res.status(200).json(r);
       })
       .catch(() => res.sendStatus(500));
-});
-
-ViteExpress.listen(app, PORT, () =>
-  console.log(`Server listening at http://localhost:${PORT}`)
-);
+}
