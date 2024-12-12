1780ec29b5c516a934bd6b9b4018381f5765cf8c
Molor Margad-Erdene <molormargaderdene@gmail.com>
2024-12-12 08:45:27 +0800

    fix: typescript error

--- src/pages/reader/Reader.tsx
+++ src/pages/reader/Reader.tsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import readings from '../../readings.json';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import readings from "../../readings.json";
 
 const Reader = () => {
   const location = useLocation();
-  const { level } = location.state || { level: 'N5' };
-  const [readingText, setReadingText] = useState('');
+  const { level } = location.state || { level: "N5" };
+  const [readingText, setReadingText] = useState("");
 
   useEffect(() => {
-    if ((readings as any)[level]) {
-      setReadingText((readings as any)[level].text);
+    if ((readings.readings as any)[level]) {
+      setReadingText((readings.readings as any)[level].text);
     } else {
-      setReadingText('Reading not found.');
+      setReadingText("Reading not found.");
     }
   }, [level]);
 
   return (
     <div className="container flex flex-col items-center p-8">
-      <h1 className="text-4xl font-bold mb-[-30px]">Reader Page - Level: {level}</h1>
+      <h1 className="text-4xl font-bold mb-[-30px]">
+        Reader Page - Level: {level}
+      </h1>
       <p className="mt-16 text-2xl leading-[70px]">{readingText}</p>
     </div>
   );
 };
 
-export default Reader; 
+export default Reader;
