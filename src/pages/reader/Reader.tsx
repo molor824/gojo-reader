79fd9a61b86cc5cc31e4c61e36e45ba82c1178c9
molor824 <molormargaderdene@gmail.com>
2024-12-16 08:47:38 +0800

    fix: move files move Read.tsx to src/pages/read move Reader.tsx to src/pages/reader refactor: remove hooks from reader

--- src/pages/reader/Reader.tsx
+++ src/pages/reader/Reader.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import readings from "../../readings.json";
 import { JapanText } from "../../components/JapanText";
@@ -6,20 +5,14 @@ import { JapanText } from "../../components/JapanText";
 const Reader = () => {
   const location = useLocation();
   const { level } = location.state || { level: "N5" };
-  const [readingText, setReadingText] = useState("");
-
-  useEffect(() => {
-    if ((readings.readings as any)[level]) {
-      setReadingText((readings.readings as any)[level].text);
-    } else {
-      setReadingText("Reading not found.");
-    }
-  }, [level]);
+  const reading = !!(readings.readings as any)[level]
+    ? (readings.readings as any)[level].text
+    : "Reading not found.";
 
   return (
     <div className="container flex flex-col gap-4 items-center p-8">
       <h1 className="text-4xl font-bold">Reader Page - Level: {level}</h1>
-      <JapanText text={readingText} />
+      <JapanText text={reading} />
     </div>
   );
 };
